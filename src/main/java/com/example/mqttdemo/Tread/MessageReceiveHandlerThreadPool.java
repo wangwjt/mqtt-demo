cae68d7192595ea78510da2f94c80b1e2e75b0a5
wangwjt <wangwjt1121@126.com>
2021-02-15 19:59:09 +0800

    优化线程池，不用他的构造

--- src/main/java/com/example/mqttdemo/Tread/MessageReceiveHandlerThreadPool.java
+++ src/main/java/com/example/mqttdemo/Tread/MessageReceiveHandlerThreadPool.java
@@ -1,8 +1,7 @@
 package com.example.mqttdemo.Tread;
 
 import java.util.Objects;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @description: 消息处理线程池(懒汉式)
@@ -12,6 +11,16 @@ import java.util.concurrent.Executors;
 public class MessageReceiveHandlerThreadPool {
 
     private static ExecutorService pool;
+
+    /**
+     * 线程数
+     */
+    private static int THREAD_NUM = 8;
+
+    /**
+     * 阻塞队列最大长度
+     */
+    private static int MAX_QUEUE_SIZE = 2000;
 
     private MessageReceiveHandlerThreadPool() {
     }
@@ -23,7 +32,11 @@ public class MessageReceiveHandlerThreadPool {
         if (Objects.isNull(pool)) {
             synchronized (MessageReceiveHandlerThreadPool.class) {
                 if (Objects.isNull(pool)) {
-                    pool = Executors.newFixedThreadPool(4);
+                    // 防止线程没处理完而持续接收消息
+                    pool = new ThreadPoolExecutor(THREAD_NUM,
+                            THREAD_NUM, 0L, TimeUnit.MILLISECONDS,
+                            new ArrayBlockingQueue<Runnable>(MAX_QUEUE_SIZE),
+                            new ThreadPoolExecutor.CallerRunsPolicy());
                 }
             }
         }
